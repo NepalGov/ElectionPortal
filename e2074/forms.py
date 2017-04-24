6a8a3d475b65bf7c4bf200e9785753a0ff61f850
Sagar Devkota <devkota@bloggernepal.com>
2017-04-24 19:04:41 +0545

    Fixing the photo upload problem and form some design and validation

--- e2074/forms.py
+++ e2074/forms.py
@@ -12,16 +12,19 @@ class WpZoneForm(forms.ModelForm):
         fields = {'name'}
 
 class WpDistrictForm(forms.ModelForm):
+    field_order = ['name','zone','vdc','municipality','population','voters']
     class Meta:
         model = District
         fields = {'name','zone','vdc','municipality','population','voters'}
 
 class WpPoliticaldivForm(forms.ModelForm):
+    field_order = ['name','zone','district','group']
     class Meta:
         model = Politicaldiv
         fields = {'zone','district','name','group'}
 
 class WpCandidateForm(forms.ModelForm):
+    field_order = ['name','party','zone','district','politicaldiv','gender','votes','status','age','criminalcase','photo','about']
     class Meta:
         model = Candidate
         fields = {'name','party','zone','district','politicaldiv','gender','votes','status','age','criminalcase','photo','about'}
